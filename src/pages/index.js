fd1c5382261258838636b56073b6f2bd16b4d40f
Jason Rodrigues <>
2023-08-12 03:41:03 +0400

    update path to have no docs

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,7 +17,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/intro">
+            to="/introduction">
             Check out the d00bai Guide!
           </Link>
         </div>
