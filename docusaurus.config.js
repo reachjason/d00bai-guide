5577b25392e9317b6dcdc8815b02bd87592765e4
Jason Rodrigues <>
2023-08-12 14:31:46 +0400

    docs-only mode. disabled blog

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -38,20 +38,21 @@ const config = {
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
           routeBasePath: '/',
+          sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
             'https://github.com/reachjason/d00bai-guide',
         },
-        blog: {
-          showReadingTime: true,
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/reachjason/d00bai-guide',
-        },
+        blog: false, // added to disable blog 
+//        {
+//          showReadingTime: true,
+//          // Please change this to your repo.
+//          // Remove this to remove the "edit this page" links.
+//          editUrl:
+//            'https://github.com/reachjason/d00bai-guide',
+//        },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
         },
